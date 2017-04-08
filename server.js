94a40f16599a60b85b24ea33c6c6116ac69187f8
Krakenfall <krakenfall@gmail.com>
2017-04-08 11:26:12 -0700

    Updated root redirect to taco-bot-web default

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 // Handle root
 app.get('/', function(req, res) {
-	res.redirect(`http://${config.domain}/list.html`);
+	res.redirect(`http://${config.domain}/index.html`);
 });
 
 // Return log, should this be an admin call?
